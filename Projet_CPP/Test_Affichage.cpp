dfff7798912c2d02b51f05e9da4bad222ed2fb4b
Worzane <38124688+Worzane@users.noreply.github.com>
2018-04-17 23:20:39 +0200

    Add files via upload
    Ajout des tests pour vérifier le bon fonctionnement des calculs et pour gérer les erreurs (division par 0, chaine de caractère return Error) . A implémenter quand l'algorithme de calcul sera fait

--- Projet_CPP/Test_Affichage.cpp
+++ Projet_CPP/Test_Affichage.cpp
@@ -1,3 +1,5 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include <iostream>
 #include "Catch.hpp"
 #include "Affichage.hpp"
@@ -16,4 +18,25 @@ TEST_CASE( "Test_Classe_Affichage", "Test_Classe_Affichage" )
 
 	CHECK(test.get_arg()==3.4);
 	CHECK(test.get_arg()==1.5);
+}
+
+/*Test la fonction "Oprérations"*/
+TEST_CASE("Test de calculs","Calcultatrice")
+{
+	Affichage Calculatrice;
+	REQUIRE (Calculatrice.calcul("5 4 +")=="9");
+	REQUIRE (Calculatrice.calcul("5 3 4 * +")=="35");
+	REQUIRE (Calculatrice.calcul("4 3 * 8 2 * -")=="-4");
+
+}
+
+TEST_CASE("Error","Test du retour d'erreur")
+{
+	Affichage Calculatrice;
+	/*Retourne une erreur si un nombre est divisé par 0*/
+	REQUIRE(Calculatrice.calcul("3/0")=="ERROR");
+	/*Ajouter un test pour check si la fenetre d'erreur s'ouvre correctement */
+
+	/* Retourne une erreur si autre chose qu'un nombre ou qu'un opérateur tente d'être écrit dans la pile de calculs*/
+	REQUIRE(Calculatrice.calcul("xxx")=="ERROR Chaine");
 }
